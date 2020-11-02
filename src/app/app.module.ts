b88c6f9e3cb8adeabbeab3d2d2c457fbd6fe9584
mutoni vivine <mutoniv5@gmail.com>
2020-11-02 20:50:37 +0200

    form component is created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import {FormsModule} from "@angular/forms";
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { FormComponent } from './form/form.component';
+import { DetailsComponent } from './details/details.component';
+import { HighlightDirective } from './highlight.directive';
+import { DaysPassedPipe } from './days-passed.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    FormComponent,
+    DetailsComponent,
+    HighlightDirective,
+    DaysPassedPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+   FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
